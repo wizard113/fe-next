d09e59300a76bb9e52a452bd26711bb9a2e12670
wizard113 <yeong.rok.lee@gmail.com>
2026-07-28 12:00:35 +0900

    2nd publish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from 'next/image';
+import styles from './page.module.css';
 
 export default function Home() {
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL ?? '(미설정)';
+  const blobUrl = process.env.NEXT_PUBLIC_BLOB_URL ?? '(미설정)';
   return (
     <div className={styles.page}>
       <main className={styles.main}>
@@ -14,27 +16,13 @@ export default function Home() {
           priority
         />
         <div className={styles.intro}>
-          <h1>To get started, edit the page.tsx file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+          <h3>Next.js 앱서비스 단독 배포</h3>
+          <ol>
+            <li>API_URL: {apiUrl}</li>
+            <li>BLOB_URL: {blobUrl}</li>
+          </ol>
         </div>
+
         <div className={styles.ctas}>
           <a
             className={styles.primary}
